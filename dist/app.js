baad5dadf4887eaf98481208ec969db6a6ae1c66
Ivan Blažević <ivan.blazevic97@gmail.com>
2020-09-23 15:43:29 +0200

    initial TypeScript classes and fetch setup
    took a while...

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,40 @@
 "use strict";
-console.log("Hello world");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const parsedArray = [];
+class FileData {
+    constructor(path) {
+        this.filePath = path;
+    }
+    getFile() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield fetch(this.filePath);
+            return response.text();
+        });
+    }
+}
+class Parser {
+    constructor(fileData) {
+        this.data = fileData;
+    }
+    parse() {
+        return this.data;
+    }
+}
+function TSParser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const file1 = new FileData("primjer1.txt");
+        const file2 = new FileData("primjer2.txt");
+        const parsedData = yield file1.getFile().then(data => new Parser(data).parse());
+        console.log(parsedArray);
+    });
+}
+document.addEventListener("DOMContentLoaded", TSParser);
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,37 @@
-console.log("Hello world");
+const parsedArray: string[] = [];
+
+class FileData {
+    filePath: string;
+
+    constructor(path: string) {
+        this.filePath = path;
+    }
+
+    public async getFile() {
+        const response = await fetch(this.filePath);
+        return response.text();
+    }
+}
+
+class Parser {
+    data: string;
+
+    constructor(fileData: string) {
+        this.data = fileData;
+    }
+
+    public parse() {
+        return this.data;
+    }
+}
+
+async function TSParser() {
+    const file1 = new FileData("primjer1.txt");
+    const file2 = new FileData("primjer2.txt");
+
+    const parsedData = await file1.getFile().then(data => new Parser(data).parse());
+
+    console.log(parsedArray);
+}
+
+document.addEventListener("DOMContentLoaded", TSParser);
