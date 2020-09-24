da2e8566244f3cbb5ab4eace2219f51c6e961d9d
Ivan Blažević <ivan.blazevic97@gmail.com>
2020-09-24 11:08:00 +0200

    parser working
    v1 implemented with 2 classes, one of which is pretty big and contains all methods for parsing

--- dist/app.js
+++ dist/app.js
@@ -10,8 +10,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const parsedArray = [];
 class FileData {
-    constructor(path) {
-        this.filePath = path;
+    constructor(filePath) {
+        this.filePath = filePath;
     }
     getFile() {
         return __awaiter(this, void 0, void 0, function* () {
@@ -21,18 +21,91 @@ class FileData {
     }
 }
 class Parser {
-    constructor(fileData) {
-        this.data = fileData;
+    constructor(data) {
+        this.data = data;
+        this.curlies = [];
+        this.tempData = this.data.replace(/[¶\r\n\t]/g, " ").replace(/[”]/g, "\"");
+        this.curlies = this.getCurlies(this.tempData);
     }
-    parse() {
-        return this.data;
+    parseData() {
+        for (let i = 0; i < this.curlies.length; i++) {
+            let elementType = "";
+            let attributes = [];
+            let elementContent = "";
+            let obj = {};
+            let beginning = this.tempData.indexOf(this.curlies[i]);
+            elementType = this.getWord(this.tempData.substring(beginning + 1, this.tempData.indexOf("}")));
+            let attributesExist = this.getAttributes(this.curlies[i]);
+            if (attributesExist) {
+                this.parseAttributes(this.curlies[i], attributesExist, attributes);
+            }
+            if (!this.curlies[i].includes("/}")) {
+                elementContent = this.tempData.substring(this.tempData.indexOf("}"), this.tempData.indexOf("{/")).slice(1);
+            }
+            else {
+                elementContent = "";
+            }
+            obj["type"] = elementType;
+            if (attributes.length > 0) {
+                obj["attributes"] = attributes;
+            }
+            if (elementContent !== "") {
+                obj["content"] = elementContent;
+            }
+            parsedArray.push(obj);
+            this.tempData = this.tempData.slice(this.tempData.indexOf(this.curlies[i + 1])).trim();
+        }
+    }
+    getCurlies(data) {
+        let pattern = /{([^}]+)}/g;
+        return data.match(pattern).filter(element => !(element.includes("{/")));
+    }
+    getWord(elementType) {
+        let pattern = /\b(\w*\w*)\b/g;
+        let word = elementType.match(pattern).toString();
+        return word.substring(0, word.indexOf(","));
+    }
+    getAttributes(elementAttributes) {
+        let pattern = /(\w+)[=]/g;
+        let attributes = elementAttributes.match(pattern);
+        if (attributes) {
+            return attributes.toString();
+        }
+    }
+    parseAttributes(element, attributesExist, attributes) {
+        while (attributesExist) {
+            let attributeStart = element.indexOf(" ");
+            let equalSign = element.indexOf("=");
+            let tmpAtrribute = element.substring(attributeStart, equalSign).trim();
+            let tmpValue = this.getBetweenQuotes(element);
+            element = element.slice(element.indexOf('"', equalSign + 2) + 1).trim();
+            if (attributesExist.indexOf(",") > -1) {
+                attributesExist = attributesExist.slice(attributesExist.indexOf(",") + 1);
+            }
+            else {
+                attributesExist = "";
+            }
+            attributes.push(tmpAtrribute + ": " + tmpValue);
+        }
+    }
+    getBetweenQuotes(attributeValue) {
+        let pattern = /[”"'](.*?)[”"']/g;
+        attributeValue = attributeValue.match(pattern).toString();
+        if (attributeValue.toString().includes(",")) {
+            attributeValue = attributeValue.slice(0, attributeValue.indexOf(","));
+        }
+        return attributeValue.slice(1, -1).trim();
+    }
+    pushToArray(parsedData) {
+        return parsedData;
     }
 }
 function TSParser() {
     return __awaiter(this, void 0, void 0, function* () {
         const file1 = new FileData("primjer1.txt");
         const file2 = new FileData("primjer2.txt");
-        const parsedData = yield file1.getFile().then(data => new Parser(data).parse());
+        const parsedFile1 = yield file1.getFile().then(data => new Parser(data).parseData());
+        const parsedFile2 = yield file2.getFile().then(data => new Parser(data).parseData());
         console.log(parsedArray);
     });
 }
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-const parsedArray: string[] = [];
+interface parsedObject {
+    type?: string;
+    attributes?: string[];
+    content?: string;
+}
+
+const parsedArray: Array<parsedObject> = [];
 
 class FileData {
-    filePath: string;
 
-    constructor(path: string) {
-        this.filePath = path;
+    constructor(private filePath: string) { // Parameter properties, squeezing three-line declarations in one line within constructor
     }
 
     public async getFile() {
@@ -14,14 +18,100 @@ class FileData {
 }
 
 class Parser {
-    data: string;
+    private curlies: string[] = [];
+    private tempData: string;
 
-    constructor(fileData: string) {
-        this.data = fileData;
+    constructor(private data: string) {
+        this.tempData = this.data.replace(/[¶\r\n\t]/g, " ").replace(/[”]/g, "\"");
+        this.curlies = this.getCurlies(this.tempData);
     }
 
-    public parse() {
-        return this.data;
+    parseData() {
+        for (let i = 0; i < this.curlies.length; i++) {
+            let elementType = "";     
+            let attributes: string[] = []; 
+            let elementContent = "";
+            let obj: parsedObject = {};
+
+            let beginning = this.tempData.indexOf(this.curlies[i]);
+            elementType = this.getWord(this.tempData.substring(beginning + 1, this.tempData.indexOf("}")));
+
+            let attributesExist = this.getAttributes(this.curlies[i]);
+            if (attributesExist) {
+                this.parseAttributes(this.curlies[i], attributesExist, attributes);
+            }
+
+            if (!this.curlies[i].includes("/}")) {                           
+                elementContent = this.tempData.substring(this.tempData.indexOf("}"), this.tempData.indexOf("{/")).slice(1);   
+            } else {                                                                                           
+                elementContent = "";                                                                
+            }
+
+            obj["type"] = elementType;
+            if (attributes.length > 0) {
+                obj["attributes"] = attributes;
+            }
+            if (elementContent !== "") {
+                obj["content"] = elementContent;
+            }
+            parsedArray.push(obj);
+
+            this.tempData = this.tempData.slice(this.tempData.indexOf(this.curlies[i + 1])).trim();
+        }
+    }
+
+    private getCurlies(data: string) {
+        let pattern = /{([^}]+)}/g;
+        return data.match(pattern)!.filter(element => !(element.includes("{/")));
+    }
+
+    private getWord(elementType: string) {
+        let pattern = /\b(\w*\w*)\b/g;                    
+        let word = elementType.match(pattern)!.toString();
+        return word.substring(0, word.indexOf(","));
+    }
+
+    private getAttributes(elementAttributes: string) {
+        let pattern = /(\w+)[=]/g;
+        let attributes = elementAttributes.match(pattern);
+
+        if (attributes) {
+            return attributes.toString();
+        }
+    }
+
+    private parseAttributes(element: string, attributesExist: string, attributes: string[]) {
+
+        while(attributesExist) {    
+            let attributeStart = element.indexOf(" ");      
+            let equalSign = element.indexOf("=");          
+            let tmpAtrribute = element.substring(attributeStart, equalSign).trim();  
+            let tmpValue = this.getBetweenQuotes(element);      
+
+            element = element.slice(element.indexOf('"', equalSign + 2) + 1).trim();
+
+            if (attributesExist.indexOf(",") > -1) {                                          
+                attributesExist = attributesExist.slice(attributesExist.indexOf(",") + 1);    
+            } else {                                                                         
+                attributesExist = "";                                                     
+            }
+
+            attributes.push(tmpAtrribute + ": " + tmpValue);                  
+        }
+    }
+
+    private getBetweenQuotes(attributeValue: string) {
+        let pattern = /[”"'](.*?)[”"']/g;
+        attributeValue = attributeValue.match(pattern)!.toString();
+
+        if (attributeValue.toString().includes(",")) {                 
+            attributeValue = attributeValue.slice(0, attributeValue.indexOf(",")); 
+        }
+        return attributeValue.slice(1, -1).trim();          
+    }
+
+    pushToArray(parsedData: string) {
+        return parsedData;
     }
 }
 
@@ -29,7 +119,8 @@ async function TSParser() {
     const file1 = new FileData("primjer1.txt");
     const file2 = new FileData("primjer2.txt");
 
-    const parsedData = await file1.getFile().then(data => new Parser(data).parse());
+    const parsedFile1 = await file1.getFile().then(data => new Parser(data).parseData());
+    const parsedFile2 = await file2.getFile().then(data => new Parser(data).parseData());
 
     console.log(parsedArray);
 }
